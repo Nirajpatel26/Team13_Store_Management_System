c3dd08b794fb49fb541192fcf2cb6e7fd97debc2
NIRAJ <patel.niraju@northeastern.edu>
2024-12-03 22:43:12 -0500

    Upadtes

--- Data Insertion/MANAGER_USER_INSERTION.sql
+++ Data Insertion/MANAGER_USER_INSERTION.sql
@@ -12,11 +12,11 @@ END;
 
 -- Insert into employee
 BEGIN
-  APP_ADMIN.INSERT_EMPLOYEE(1, 'Rajesh', 'Kumar', 'rajesh.kumar@example.com', 9876543210, '123 Main St, Mumbai', 'Manager', TO_DATE('2022-05-15', 'YYYY-MM-DD'), 75000.00);
-  APP_ADMIN.INSERT_EMPLOYEE(2, 'Priya', 'Sharma', 'priya.sharma@example.com', 9876543211, '456 Oak St, Delhi', 'Marketing Manager', TO_DATE('2022-06-01', 'YYYY-MM-DD'), 65000.00);
-  APP_ADMIN.INSERT_EMPLOYEE(3, 'Amit', 'Patel', 'amit.patel@example.com', 9876543212, '789 Pine St, Bangalore', 'Inventory Manager ', TO_DATE('2022-07-01', 'YYYY-MM-DD'), 55000.00);
-  APP_ADMIN.INSERT_EMPLOYEE(4, 'Sneha', 'Gupta', 'sneha.gupta@example.com', 9876543213, '101 Elm St, Chennai', 'Cashier', TO_DATE('2022-08-01', 'YYYY-MM-DD'), 45000.00);
-  APP_ADMIN.INSERT_EMPLOYEE(5, 'Vikram', 'Singh', 'vikram.singh@example.com', 9876543214, '202 Maple St, Kolkata', 'Cashier', TO_DATE('2022-09-01', 'YYYY-MM-DD'), 50000.00);
+  APP_ADMIN.INSERT_EMPLOYEE(1, 'Rajesh', 'Kumar', 'rajesh.kumar@gmail.com', 9876543210, '123 Main St, Mumbai', 'Manager', TO_DATE('2022-05-15', 'YYYY-MM-DD'), 75000.00);
+  APP_ADMIN.INSERT_EMPLOYEE(2, 'Priya', 'Sharma', 'priya.sharma@yahoo.com', 9876543211, '456 Oak St, Delhi', 'Marketing Manager', TO_DATE('2022-06-01', 'YYYY-MM-DD'), 65000.00);
+  APP_ADMIN.INSERT_EMPLOYEE(3, 'Amit', 'Patel', 'amit.patel@gmail.com', 9876543212, '789 Pine St, Bangalore', 'Inventory Manager ', TO_DATE('2022-07-01', 'YYYY-MM-DD'), 55000.00);
+  APP_ADMIN.INSERT_EMPLOYEE(4, 'Sneha', 'Gupta', 'sneha.gupta@yahoo.com', 9876543213, '101 Elm St, Chennai', 'Cashier', TO_DATE('2022-08-01', 'YYYY-MM-DD'), 45000.00);
+  APP_ADMIN.INSERT_EMPLOYEE(5, 'Vikram', 'Singh', 'vikram.singh@outlook.com', 9876543214, '202 Maple St, Kolkata', 'Cashier', TO_DATE('2022-09-01', 'YYYY-MM-DD'), 50000.00);
 END;
 /
 
